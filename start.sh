399c2fb0efb9975ac3e02bbc4546ac9a2ae8937f
boherbatov <boherbatov@gmail.com>
2026-09-19 18:14:31 -0500

    Add access log to gunicorn execution in start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 node /pot/server/build/main.js &
 sleep 2
-exec /opt/venv/bin/gunicorn -b 0.0.0.0:${PORT:-10000} -w 1 -k gthread --threads 8 -t 180 app:app
+exec /opt/venv/bin/gunicorn -b 0.0.0.0:${PORT:-10000} -w 1 -k gthread --threads 8 -t 180 --access-logfile - app:app
